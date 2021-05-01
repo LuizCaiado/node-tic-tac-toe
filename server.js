e0b020c8c54bde3f0f36c9297c90a6b71d329cf9
Luiz Caiado <luiz.caiado@sensedia.com>
2021-05-01 16:45:09 -0300

    Turn, GetGame and reset implemented

--- server.js
+++ server.js
@@ -2,7 +2,56 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-var game = [];
+var game = [
+    ["","","",],
+    ["","","",],
+    ["","","",]
+];
+var turno = 0;
+
+function MarcarCelula (req, res){
+    let paddr = url.parse(req.url, true);
+
+    let posI = paddr.query.posI;
+    let posJ = paddr.query.posJ;
+
+
+    if(posI === undefined || posJ === undefined){
+        res.writeHead(400);
+        res.end();
+    } else if (game[posI][posJ] != ""){   
+        res.writeHead(401);
+        res.end("Campo já ocupado");
+    } else {
+        if (turno == 0){
+            game[posI][posJ] = 'X';
+            turno = 1;
+        } else {
+            game[posI][posJ] = 'O';
+            turno = 0;
+        }
+
+        res.writeHead(200);
+        res.end();
+
+    }
+
+}
+
+function GetGame(res) {
+    res.writeHead(200);
+    res.end(`${game}`);
+}
+
+function ResetGame(res){
+    game = [
+        ["","","",],
+        ["","","",],
+        ["","","",]
+    ];
+    res.writeHead(200);
+    res.end();
+}
 
 http.createServer(function(req,res) {
     var paddr = url.parse(req.url, true);
@@ -11,7 +60,7 @@ http.createServer(function(req,res) {
     if(paddr.pathname === '/turn'){
         MarcarCelula(req,res);
     } else if(paddr.pathname === '/game'){
-        getGame(req,res);
+        GetGame(res);
     } else if(paddr.pathname === '/reset'){
         ResetGame(res);
     } else {
